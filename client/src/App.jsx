5bee7a292c740fe0e7817277720b8a19c0201235
jeramy <jeramy.reid84@gmail.com>
2024-10-09 11:24:46 -0700

    app structure initial set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import Header from './components/header/Header.jsx';
+import Qa from './components/qa/Qa.jsx';
+import Related from './components/relatedComparison/Related.jsx';
+import Comparison from './components/relatedComparison/Comparison.jsx';
+import Rating from './components/reviewRating/Rating.jsx';
+import Selector from './components/styleSelect/Selector.jsx';
 
 const App = () => {
   return (
     <div className='app'>
-      <p>Cats</p>
+      <Header />
+      <Selector />
+      <div className='related-comp-container'>
+        <Related />
+        <Comparison />
+      </div>
+      <div className='q-a-container'>
+        <Qa />
+      </div>
+      <div className='rating-review-container'>
+        <Rating />
+      </div>
     </div>
   )
 };
